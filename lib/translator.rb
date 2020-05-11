9ac46005eaf395e127f6488dad4a7ce4ffe19a0a
Tien Phan <tienphan1989@gmail.com>
2020-05-11 17:46:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -25,12 +25,11 @@ return "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(path, emoticon)
-  library = load_library(path)
+ library = load_library(path)
  library.each do |key,value|
-  
-   if emoticon == value[:english]
-     return value[:japanese]
+   if emoticon == value[:japanese]
+      return  key
    end
-   end
-   return "Sorry, that emoticon was not found"
+  end
+return "Sorry, that emoticon was not found"
 end
