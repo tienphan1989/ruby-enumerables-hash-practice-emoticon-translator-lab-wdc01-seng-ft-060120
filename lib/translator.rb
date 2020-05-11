31b7a08dec19e00c48079a0c05bf8368399bf936
Tien Phan <tienphan1989@gmail.com>
2020-05-11 14:47:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,25 @@
-# require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(path)
+  emoticon_hash = Hash.new
+
+   emoticon_hash[:response] = {:english=> {}, :japanese=> {}}
+
+   library = YAML.load_file(path)
+   library.each do |key, value|
+   emoticon_hash[response] = key
+   response[:english] = value[0]
+   response[:japanese] = value[1]
+   
+  end
+emoticon_hash 
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(path, emoticon)
+
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(path, emoticon)
+
 end
