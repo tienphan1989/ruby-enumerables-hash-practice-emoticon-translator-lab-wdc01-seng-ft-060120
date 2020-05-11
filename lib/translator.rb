91e9c2f8255a84ed5029d16191df79fbb7530f14
Tien Phan <tienphan1989@gmail.com>
2020-05-11 16:10:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,12 +4,12 @@ require 'pry'
 def load_library(path)
     emoticon_library = YAML.load_file(path)
   sorted_emoticons = {
-    :get_meaning => {:english => {},
+    'get_meaning' => {:english => {},
                     :Japanese => {}
                     }
   }
   emoticon_library.each do |key, value|
-  sorted_emoticons[:get_meaning] = key
+  sorted_emoticons['get_meaning'] = key
   sorted_emoticons[:get_meaning][:english] = value[0]
   sorted_emoticons[:get_meaning][:english] = value[1]
 binding.pry
