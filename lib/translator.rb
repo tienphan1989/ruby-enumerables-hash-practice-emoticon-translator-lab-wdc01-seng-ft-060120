0b73edc13fde8f9b5b536ecd5a1de0769ea18bc4
Tien Phan <tienphan1989@gmail.com>
2020-05-11 15:36:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,19 +8,11 @@ def load_library(path)
     hashed["get_meaning"] = meaning
     hashed["get_meaning"][:english] = value[0] 
     hashed["get_meaning"][:japanese] = value[1]
+    bind.pry
   end
   hashed
 end
-  hashed = {
-    'get_meaning': {}
-  }
-  emoticon_library.each do |key, value|
-    hashed[:get_meaning][value[1]] = key #Japanese emoticon is the key; name/meaning is the value
-    hashed[:get_emoticon][:english] = value[1]
-    hashed[:]
-    #English emoticon is the key; Japanese emoticon is the value
-  end
-  sorted_emoticons
+
 
 def get_japanese_emoticon(path, emoticon)
   emoticon_hash = load_library(path)
