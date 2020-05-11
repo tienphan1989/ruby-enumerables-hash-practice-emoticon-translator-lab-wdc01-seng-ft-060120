37870f1c5b419c68706b607bc5a7834b86692565
Tien Phan <tienphan1989@gmail.com>
2020-05-11 15:38:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,19 +15,9 @@ end
 
 
 def get_japanese_emoticon(path, emoticon)
-  emoticon_hash = load_library(path)
-  result = emoticon_hash["get_emoticon"][emoticon]
-  if result == nil
-    result = "Sorry, that emoticon was not found" 
-  end
-  result
+
 end
 
 def get_english_meaning(path, emoticon)
-  emoticon_hash = load_library(path)
-  result = emoticon_hash["get_meaning"][emoticon]
-  if result == nil
-    result = "Sorry, that emoticon was not found" 
-  end
-  result
+
 end
