b33fa86aa51a2fea010d3d9dc5a48fe2755a07f6
Tien Phan <tienphan1989@gmail.com>
2020-05-11 17:12:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,8 +11,9 @@ def load_library(file_path)
     english, japanese = array
     library["meaning"][:english] = array[0]
     library["meaning"][:japanese] = array[1]
-    binary.pry
+    
   end
+  binary.pry
   library
 end
 
