59d767cbb4e02db20be9d0b3feb1eb67521cadb5
Tien Phan <tienphan1989@gmail.com>
2020-05-11 15:30:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,12 +6,22 @@ def load_library(path)
   YAML.load_file(path).each do |meaning, emoticons|
     english, japanese = emoticons
     hashed["get_meaning"] = meaning
-    hashed["get_meaning"] = {:english => 
+    hashed["get_meaning"][:english] = {:english => 
     hashed["get_emoticon"][english] = japanese
     #hashed["get_meaning"][japanese] = meaning
   end
   hashed
 end
+  hashed = {
+    'get_meaning': {}
+  }
+  emoticon_library.each do |key, value|
+    hashed[:get_meaning][value[1]] = key #Japanese emoticon is the key; name/meaning is the value
+    hashed[:get_emoticon][:english] = value[1]
+    hashed[:]
+    #English emoticon is the key; Japanese emoticon is the value
+  end
+  sorted_emoticons
 
 def get_japanese_emoticon(path, emoticon)
   emoticon_hash = load_library(path)
