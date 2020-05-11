fbb3aeb6ed40d2002d41224b03b75f6356a62cef
Tien Phan <tienphan1989@gmail.com>
2020-05-11 15:41:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,10 +7,10 @@ def load_library(path)
     'get_meaning': {},
     'get_emoticon': {}
   }
-  binding.pry
   emoticon_library.each do |key, value|
     sorted_emoticons[:get_meaning][value[1]] = key #Japanese emoticon is the key; name/meaning is the value
     sorted_emoticons[:get_emoticon][value[0]] = value[1] #English emoticon is the key; Japanese emoticon is the value
+  binding.pry
   end
   sorted_emoticons
 end
